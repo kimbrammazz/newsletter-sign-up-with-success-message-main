7f293e409972aa2bbb0b93bbeadcf5fcb8a9ddda
Kim Bramwell Mazzarella <kimbrammazz@gmail.com>
2024-04-15 14:49:18 +1000

    success message

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("hello");
+const signup = document.querySelector("#signup");
+const success = document.querySelector("#success");
+
+window.addEventListener("DOMContentLoaded", () => {
+	success.style.display = "none";
+});
